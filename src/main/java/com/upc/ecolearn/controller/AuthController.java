d503bc0c44766efef560c8c2a59322a533407c26
Joaquin Arévalo <joaquin.arevalo42@gmail.com>
2026-09-13 20:42:36 -0500

    refactor: eliminar @CrossOrigin y OPTIONS handlers redundantes en AuthController

--- src/main/java/com/upc/ecolearn/controller/AuthController.java
+++ src/main/java/com/upc/ecolearn/controller/AuthController.java
@@ -8,11 +8,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
@@ -28,15 +26,5 @@ public class AuthController {
     public ResponseEntity<AuthResponse> login(
             @Valid @RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
-    }
-
-    @RequestMapping(value = "/register", method = RequestMethod.OPTIONS)
-    public ResponseEntity<?> optionsRegister() {
-        return ResponseEntity.ok().build();
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.OPTIONS)
-    public ResponseEntity<?> optionsLogin() {
-        return ResponseEntity.ok().build();
     }
 }
